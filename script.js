b7836fdfe45818bdfcab96c280f559353065162f
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-05-17 18:34:40 -0300

    Testando retorno das mensagem de validação do CPF

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 console.log('Iniciando o arquivo JS');
 
+function validaCPF(cpf){
+  return false;
+}
+
 function validacao(){
   console.log('Iniciando validação CPF');
+
+  var cpf = document.getElementById('cpf_digitado').value;
+  console.log(cpf)
+
+  var resultadoValidacao = validaCPF(cpf);
+
+  if(resultadoValidacao) {
+    document.getElementById('success').style.display = 'block'
+  } else {
+    document.getElementById('error').style.display ='block'
+  }
+
 }
